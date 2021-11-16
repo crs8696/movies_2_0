e70534f8b9c9e832888b6c94cea96b0bc243357c
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-16 00:04:47 +0000

    Generate graphiti resources for reviews

---
+++ app/resources/review_resource.rb
@@ -0,0 +1,16 @@
+class ReviewResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :movie_id, :integer
+  attribute :director_id, :integer
+  attribute :year, :string
+  attribute :duration, :string
+  attribute :review_content, :integer
+  attribute :cast, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
