426c32613ffa5abccacb3bef31716ad6544ab776
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-16 00:04:36 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,17 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :integer
+  attribute :year, :date
+  attribute :duration, :string
+  attribute :description, :string
+  attribute :director_id, :integer
+  attribute :details, :integer
+  attribute :cast, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
