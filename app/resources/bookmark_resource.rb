b2f259707a547224c22fb3349d06687713f5df45
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-16 00:04:44 +0000

    Generate graphiti resources for bookmarks

---
+++ app/resources/bookmark_resource.rb
@@ -0,0 +1,11 @@
+class BookmarkResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_bookmarks, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
