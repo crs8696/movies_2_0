1db96227c596c7944ed674a1593e888d217e25eb
anonymous anonymous <celina.stewart@kellogg.northwestern.edu>
2021-11-16 00:03:53 +0000

    Whitelist columns in Actor for ActiveAdmin

--- app/admin/actors.rb
+++ app/admin/actors.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Actor do
 
+ permit_params :image, :date_of_birth, :name, :details
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
